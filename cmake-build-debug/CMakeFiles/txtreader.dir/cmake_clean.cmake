9c276035b41147f991876dda47514ac986ecf279
Astral <1245474611@qq.com>
2020-12-09 13:16:18 +0800

    hello git

---
+++ cmake-build-debug/CMakeFiles/txtreader.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/txtReader.dir/main.c.obj"
+  "libtxtReader.dll.a"
+  "txtReader.exe"
+  "txtReader.exe.manifest"
+  "txtReader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/txtReader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
